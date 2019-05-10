ef51abb4adce801cc9b7926e8752e278cdfe6d73
ThePhisics101 <thephisics101@gmail.com>
2019-05-10 20:11:38 +0300

    v:a0.4.2

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -7,11 +7,12 @@ class Game extends Component {
   activeKeys = new Set();
 
   playerVector = {a: 0, l: 0};
+  gamepadState = undefined;
 
   state = {
     player: {x: 0, y: 0, a: 0},
     paused: true,
-    gamepad: 0
+    gamepad: 0,
   };
 
   //region Component Events
@@ -63,22 +64,38 @@ class Game extends Component {
       const v = this.playerVector;
       switch (method) {
         case 0:
-          if (this.props.gamepad && gamepad > 0) {
-            let {axes: [y1], buttons: [,,,,,, {value: lt}, {value: rt}]} = navigator.getGamepads()[0];
-            if (Math.abs(y1) < 0.1) y1 = 0; if (Math.abs(y1) > 0.9) y1 = Math.sign(y1);
-            v.a += y1;
-            v.l -= 0.1 * (lt - rt);
+          let x, y;
+          if (this.props.gamepad && gamepad > 0 && (this.gamepadState = navigator.getGamepads()[0])) {
+            // this.gamepadState = navigator.getGamepads()[0]; // TODO: check if this actually works
+            let {axes: [x1], buttons: [,,,,,, {value: lt}, {value: rt}]} = this.gamepadState;
+            if (Math.abs(x1) < 0.1) x1 = 0; if (Math.abs(x1) > 0.9) x1 = Math.sign(x1);
+            // v.l -= 0.1 * (lt - rt);
+            x = 0.1 * (rt - lt);
+            // v.a += x1 * 1.5;
+            y = x1 * 1.5;
           } else if (this.activeKeys.size > 0) {
             if (["KeyW", "KeyS"].some(v => this.activeKeys.has(v)))
-              v.l += (this.activeKeys.has("KeyS") ? -1 : 1) * 0.1;
+              // v.l += (this.activeKeys.has("KeyS") ? -0.75 : 1) * 0.1;
+              x = (this.activeKeys.has("KeyS") ? -0.75 : 1) * 0.1;
             if (["KeyD", "KeyA"].some(v => this.activeKeys.has(v)))
-              v.a += (this.activeKeys.has("KeyA") ? -1 : 1) * 1.5;
+              y = (this.activeKeys.has("KeyA") ? -1 : 1) * 1.5;
+              // v.a += (this.activeKeys.has("KeyA") ? -1 : 1) * 1.5;
           }
 
-          if (Math.abs(v.l) > 0.05) v.l -= 0.05 * Math.sign(v.l) * friction;
-          else v.l = 0;
-          if (Math.abs(v.a) > 1) v.a -= 1 * Math.sign(v.a);
-          else v.a = 0;
+          if (x) {
+            v.l += x;
+          } else {
+            if (Math.abs(v.l) > 0.2 * friction) v.l -= 0.2 * Math.sign(v.l) * friction;
+            else v.l = 0;
+          }
+
+          if (y) {
+            v.a += y;
+          } else {
+            if (Math.abs(v.a) > 0.75 * friction) v.a -= 0.75 * Math.sign(v.a) * friction;
+            else v.a = 0;
+          }
+
 
           v.l = Math.max(-5, Math.min(7.5, v.l));
           v.a = Math.max(-10, Math.min(10, v.a));
@@ -142,11 +159,24 @@ class Game extends Component {
               playerSize.w / 2 * scale * MDeg.cos(player.a),
               playerSize.w / 2 * scale * MDeg.cos(player.a + 90)
             ]));
+            const gamepad = (() => {
+              if (!debug || this.state.gamepad === 0 || !this.props.gamepad || !this.gamepadState) return undefined;
+              console.log(this.gamepadState.axes);
+              const {axes: [x1 = 0, y1 = 0, x2 = 0, y2 = 0] = [], buttons = []} = this.gamepadState;
+              console.log(buttons);
+              return <g transform={`scale(${scale}) translate(20 20)`}>
+                <circle r={10} fill="transparent" stroke="gray"/>
+                <circle r={10} cx={25} fill="transparent" stroke="gray"/>
+                <circle r={2} cx={x1*10} cy={y1*10} fill="gray"/>
+                <circle r={2} cx={x2*10+25} cy={y2*10} fill="gray"/>
+                {buttons.map((v, k) => <circle key={k} r={2} cx={k*5+40} cy={(v.value-0.5)*15} fill="gray"/>)}
+              </g>;
+            })();
             return <svg viewBox={`${-scale} ${-scale} ${size.x + 2 * scale} ${size.y + 2 * scale}`}>
               <rect width="100%" height="100%" fill="rgba(255,255,255,0.2)"/>
               <line {...startLine} stroke="green" strokeWidth={2 * scale}/>
-              <line x2={this.playerVector.l*10*scale} stroke="yellow" strokeWidth={scale}
-                    transform={`translate(${player.x} ${player.y}) rotate(${player.a}) rotate(${this.playerVector.a})`}/>
+              <line x2={this.playerVector.l*10*scale} stroke="yellow" strokeWidth={scale} transform={`translate(${player.x} ${player.y}) rotate(${player.a}) rotate(${this.playerVector.a})`}/>
+              {gamepad}
               <image
                 x={-imageSize.w /2 *scale} y={-imageSize.h /2 *scale} height={imageSize.h * scale} xlinkHref={playerSprite}
                 transform={`translate(${player.x} ${player.y}) rotate(${player.a})`}
